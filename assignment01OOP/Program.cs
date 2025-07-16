ff52128ef139c2847832445de809a557c648f8b4
martin reda <martinreda999@outlook.com>
2025-07-16 07:21:55 +0300

    --Q01

--- assignment01OOP/Program.cs
+++ assignment01OOP/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace assignment01OOP
+﻿using assignment01OOP.classes;
+
+namespace assignment01OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q 01 
+            /*1-Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. 
+             * Then, write a C# program that prints out all the days of the week using this Enum.*/
+
+            int i = 1;
+
+            foreach (WeekDays Value in Enum.GetValues(typeof(WeekDays)))
+            {
+             
+                Console.WriteLine($"Day {i} : {Value}");
+                i++;
+            }
+            #endregion
         }
     }
 }
